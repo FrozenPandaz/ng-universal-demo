18fa3fe9a83569099e22dcd59f59a74beecb95c2
Denys Dnishchenko <maden.maxi@yandex.ru>
2017-08-30 03:29:31 +0300

    Adding angular-cli support

--- src/app/+lazy/lazy.module.ts
+++ src/app/+lazy/lazy.module.ts
@@ -1,19 +1,50 @@
-import {NgModule, Component} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import { NgModule, Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PostComponent } from './post/post.component'
+import { TransferHttp } from '../../modules/transfer-http/transfer-http';
+import { Observable } from 'rxjs/Observable';
 
+interface User {
+	id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: any,
+    phone: string,
+    website: string,
+    company: any
+}
 
 @Component({
   selector: 'lazy-view',
-  template: `<h3>i'm lazy</h3>`
+  template: `
+  	<h3>i'm lazy</h3>
+  	<ul>
+		<li *ngFor="let user of users | async">
+			({{user.id}}). {{ user.name | uppercase }}
+			<br />
+			<i>{{user.email}} : {{user.phone}}</i>
+			<address>{{user.address.street}}</address>
+		</li>
+  	</ul>
+  `
 })
-export class LazyView {}
+export class LazyView implements OnInit {
+	users: Observable<Array<User>>;
+	constructor(private http: TransferHttp) { }
+	ngOnInit(){
+		this.users = this.http.get('http://jsonplaceholder.typicode.com/users').map(res => res);
+	}
+}
 
 @NgModule({
-  declarations: [LazyView],
+  declarations: [LazyView, PostComponent],
   imports: [
     RouterModule.forChild([
       { path: '', component: LazyView, pathMatch: 'full'}
-    ])
+    ]),
+    CommonModule
   ]
 })
 export class LazyModule {
--- src/app/home/home-view.component.ts
+++ src/app/home/home-view.component.ts
@@ -4,16 +4,23 @@ import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'home-view',
-  template: `<h3>{{subs | async}}</h3>`
+  template: `<h3>{{subs | async}}</h3>
+		<ul>
+			<li *ngFor="let post of posts | async">{{post.title}}</li>
+		</ul>
+  `
 })
 export class HomeView implements OnInit {
   public subs: Observable<string>;
-
+  public posts: Observable<any>;
   constructor(private http: TransferHttp) {}
 
   ngOnInit() {
     this.subs = this.http.get('http://localhost:8000/data').map(data => {
       return `${data.greeting} ${data.name}`;
+    });
+    this.posts = this.http.get('http://jsonplaceholder.typicode.com/posts').map(data => {
+    	return data;
     });
   }
 }
